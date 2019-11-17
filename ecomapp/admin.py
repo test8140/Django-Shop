731c6c4f45f929473a5a8a02d1724e3a879f3631
test8140 <test8140@ukr.net>
2019-11-17 14:41:00 +0200

    Static files, an addition to urls.py, create the first models.

--- ecomapp/admin.py
+++ ecomapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecomapp.models import Category, Brand
+
+admin.site.register(Category)
+admin.site.register(Brand)
--- ecomapp/models.py
+++ ecomapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
