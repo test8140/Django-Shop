74a51972559112e8bf554d8536c6c4475c546e9e
test8140 <test8140@ukr.net>
2019-11-19 10:07:04 +0200

    Create a product model, create a separate folder for images.

--- ecomapp/admin.py
+++ ecomapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from ecomapp.models import Category, Brand
+from ecomapp.models import Category, Brand, Product
 
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(Product)
--- ecomapp/models.py
+++ ecomapp/models.py
@@ -15,3 +15,24 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def image_folder(instance, filename):
+    filename = instance.slug + '.' + filename.split('.')[1]
+    return "{0}/{1}"  .format(instance.slug, filename)
+
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to=image_folder)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    aveilable = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
