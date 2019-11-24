0043ecd194ab729c9ede81a1524a5c160cae1ca2
test8140 <test8140@ukr.net>
2019-11-24 11:25:22 +0200

    Add a slider to the main one, create a basket.

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -64,3 +64,22 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'slug': self.slug})
+
+
+class CartItem(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1)
+    item_total = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return 'Cart item for product {0}' .format(self.product.title)
+
+
+class Cart(models.Model):
+
+    items = models.ManyToManyField(CartItem)
+    cart_total = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return str(self.slug)
